e3439369b6461c5cd39a429bc3ded424b8d4303c
Kuky <gonzalo.munoz@elun.cl>
2020-06-05 22:36:42 -0400

    Se agrega método para listar arreglo de personas

--- app.go
+++ app.go
@@ -7,16 +7,32 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Person Definition
+type Person struct {
+	ID string `json:"id, omitempty"`
+	FirstName string `json:"first_name, omitempty"`
+	LastName string `json:"last_name, omitempty"`
+	Address *Address `json:"address, omitempty"`
+}
+
+type Address struct {
+	City string `json:"city, omitempty"`
+	State string `json:"state, omitempty"`
+}
+
+// People Aray
+var people []Person
+
 func Home(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello World"))
 }
 
 func GetPeople(w http.ResponseWriter, r *http.Request){
-
+	json.NewEncoder(w).Encode(people)
 }
 
 func GetPerson(w http.ResponseWriter, r *http.Request){
-
+	
 }
 
 func CreatePerson(w http.ResponseWriter, r *http.Request){
@@ -28,12 +44,16 @@ func main(){
 	// http.HandleFunc("/", Home )
 	// Server start
 	// Router
+	people = append(people, Person{ID: "1", FirstName: "Gonzalo", LastName: "Munoz", Address: &Address{City: "Viña del Mar", State: "Valparaiso"}})
+	people = append(people, Person{ID: "2", FirstName: "Fernanda", LastName: "Munoz"})
+
 	router := mux.NewRouter()
 	// endpoints
+	router.HandleFunc("/", Home).Methods("GET")
 	router.HandleFunc("/people", GetPeople).Methods("GET")
 	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-	router.HandleFunc("/people/", CreatePerson).Methods("POST")
-	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+	// router.HandleFunc("/people/", CreatePerson).Methods("POST")
+	// router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
 	
 	log.Fatal(http.ListenAndServe(":3000", router))
 	
