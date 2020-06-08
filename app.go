1cd84291872ae7c98c1f7ba46bac49a5a3b713b0
Kuky <gonzalo.munoz@elun.cl>
2020-06-08 14:00:13 -0400

    Se agregan recursos de crud

--- app.go
+++ app.go
@@ -32,11 +32,35 @@ func GetPeople(w http.ResponseWriter, r *http.Request){
 }
 
 func GetPerson(w http.ResponseWriter, r *http.Request){
-	
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"]{
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
 }
 
 func CreatePerson(w http.ResponseWriter, r *http.Request){
+	// people_count := len(people) + 1
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(person)
+}
 
+func DeletePerson(w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"]{
+			people = append(people[:index], people[index + 1:]...)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(people)
 }
 
 func main(){
@@ -52,8 +76,8 @@ func main(){
 	router.HandleFunc("/", Home).Methods("GET")
 	router.HandleFunc("/people", GetPeople).Methods("GET")
 	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-	// router.HandleFunc("/people/", CreatePerson).Methods("POST")
-	// router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
 	
 	log.Fatal(http.ListenAndServe(":3000", router))
 	
